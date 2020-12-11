8c8944e0d14fc83ec1615eabc881d34912936b37
Emo Maat <emosbm@gmail.com>
2020-12-11 21:13:52 +0100

    Fixed that the server would not auto-restart when crashed

--- server.sh
+++ server.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-# Copyright (c) 2019 TeamSpeak Systems GmbH
-# Altered to own use
-# All rights reserved
-
 PIDFILE="pause.pid"
 SERVICE="minecraft"
 BINARY=/usr/bin/screen
@@ -10,16 +6,18 @@ PARAMETERS="-DmS ${SERVICE} java -Xms1G -Xmx5G -jar /home/games/minecraft/forge.
 # add any command line parameters you want to pass here
 
 start() {
-    # No server running at all
-    if screen -list | grep -q "$SERVICE"; then
+    # If the server is not running
+    if ! screen -list | grep -q "$SERVICE"; then
         # The server is paused, kill the waiting process
         if [ -f $PIDFILE ]; then
             kill $(cat $PIDFILE)
             rm $PIDFILE
         fi
 
+        # Start the server
         $BINARY $PARAMETERS &
 
+        # If screen hates us
         if [ $? -ne 0 ]; then
             echo "Minecraft server could not start"
             return 1
@@ -35,15 +33,15 @@ start() {
 
     # The server is paused, kill the waiting process and restart
     else
+        # Killing the pause restarts the server
         kill $(cat $PIDFILE)
         rm $PIDFILE
-        start
     fi
 }
 
 stop() {
     # If the server is running
-    if ! screen -list | grep -q "$SERVICE"; then
+    if screen -list | grep -q "$SERVICE"; then
         echo "Stopping Minecraft Server"
         $BINARY -p 0 -S $SERVICE -X eval 'stuff "save-all"\015'
         $BINARY -p 0 -S $SERVICE -X eval 'stuff "stop"\015'
@@ -53,6 +51,10 @@ stop() {
     elif [ -f $PIDFILE ]; then
         kill $(cat $PIDFILE)
         rm $PIDFILE
+
+        # Killing the pause restarts the server, so we need to stop it again
+        # This closes the screen session mercilessly, otherwise we need to wait before the server is up again
+        screen -p 0 -S $SERVICE -X quit
     else
         echo "No server running"
         return 1
@@ -63,16 +65,16 @@ stop() {
 
 pause(){
     # If it is the server running
-    if ! screen -list | grep -q "$SERVICE" ; then
+    if screen -list | grep -q "$SERVICE" ; then
         stop
+
+        # Give the server a moment to stop
         sleep 10
 
+        # Start ncat and save its pid so we can kill it later, for freeing the port
+        # Use a & so it runs in the background, releasing the terminal or cron (if thats a thing)
         # Upon a connect, ncat will exit by sending "", after which the server starts
-        echo "" | ncat -l 25565 > /dev/null && rm $PIDFILE && start &
-
-        # We move the previous command to the background
-        # and save the pid of the whole command
-        echo $! > $PIDFILE
+        (echo "" | $(ncat -l 25565 & echo $! > $PIDFILE) > /dev/null && rm $PIDFILE && start) &
     fi
 
     return 0
@@ -81,9 +83,11 @@ pause(){
 try_stop(){
     $BINARY -p 0 -S $SERVICE -X eval 'stuff "list\015"'
 
-    # There should never be "There are 0/20 players online:" because a new latest.log should be generated every time on start
+    # There should never be "There are 0/20 players online:"
+    # because a new latest.log will be clean when restarting
     online="$(grep -n "There are 0/20 players online:" logs/latest.log)"
 
+    # If we did notice no one is online, pause
     if [ $online = "" ]; then
         pause
     fi
@@ -91,7 +95,7 @@ try_stop(){
     return 0
 }
 
-# change directory to the scripts location
+# Change directory to the scripts location, prevents a bug when using as service
 cd $(dirname $([ -x "$(command -v realpath)" ] && realpath "$0" || readlink -f "$0"))
 
 case "$1" in
