1333352e665b5af32dd2926d048207fe424272fd
Emo Maat <emomaat@Bettan.fritz.box>
2021-02-06 17:35:11 +0100

    Fixed incorrect screen checks

--- server.sh
+++ server.sh
@@ -5,16 +5,17 @@ RESTART_IF_STOPPED=0
 OPTIMALISATIONS="-XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true"
 
 PIDFILE="pause.pid"
-SERVICE="minecraft"
+SERVICE="vanilla"
 PORT=25565
 BINARY=/usr/bin/screen
+JAR="/home/games/vanilla/paper-*.jar"
 
 # Add any command line parameters you want to pass here
-PARAMETERS="-DmS ${SERVICE} java -Xms5G -Xmx5G ${OPTIMALISATIONS} -jar /home/games/minecraft/forge.jar nogui"
+PARAMETERS="-DmS ${SERVICE} java -Xms1G -Xmx1G ${OPTIMALISATIONS} -jar ${JAR} nogui"
 
 start() {
     # If the server is not running
-    if ! screen -list | grep -q "$SERVICE"; then
+    if screen -list | grep -q "$SERVICE"; then
         # The server is paused, kill the waiting process
         if [ -f $PIDFILE ]; then
             kill $(cat $PIDFILE)
@@ -26,7 +27,7 @@ start() {
 
         # If screen hates us
         if [ $? -ne 0 ]; then
-            echo "Minecraft server could not start"
+            echo "Server could not start"
             return 1
         fi
 
@@ -48,8 +49,8 @@ start() {
 
 stop() {
     # If the server is running
-    if screen -list | grep -q "$SERVICE"; then
-        echo "Stopping Minecraft Server"
+    if ! screen -list | grep -q "$SERVICE"; then
+        echo "Stopping server..."
         $BINARY -p 0 -S $SERVICE -X eval 'stuff "stop"\015'
 
     # The server is paused, kill the waiting process
@@ -69,8 +70,8 @@ stop() {
 }
 
 pause(){
-    # If it is the server running
-    if screen -list | grep -q "$SERVICE" ; then
+    # If the server is running
+    if ! screen -list | grep -q "$SERVICE" ; then
         stop
 
         # Give the server a moment to stop
@@ -79,7 +80,7 @@ pause(){
         # Start ncat and save its pid so we can kill it later, for freeing the port
         # Use a & so it runs in the background, releasing the terminal or cron (if thats a thing)
         # Upon a connect, ncat will exit by sending "", after which the server starts
-        (echo "" | $(ncat -l $PORT & echo $! > $PIDFILE) > /dev/null; rm $PIDFILE && start) &
+        (echo "" | $(ncat -l $PORT & echo $! > $PIDFILE) > ./connect.ncat.log; rm $PIDFILE && start) &
     fi
 
     return 0
@@ -87,7 +88,7 @@ pause(){
 
 try_pause(){
     # If the server is not running and we are note waiting for a new connection
-    if [ $RESTART_IF_STOPPED -eq 1 ] && [ ! -f $PIDFILE ] && ! screen -list | grep -q "$SERVICE"; then
+    if [ $RESTART_IF_STOPPED -eq 1 ] && [ ! -f $PIDFILE ] && screen -list | grep -q "$SERVICE"; then
         start
     else
         $BINARY -p 0 -S $SERVICE -X eval 'stuff "list\015"'
@@ -97,7 +98,7 @@ try_pause(){
         online="$(grep -n "There are 0/[0-9]* players online:" logs/latest.log)"
 
         # If we did notice no one is online, pause
-        if [ "$online" = "" ]; then
+        if [ "$online" != "" ]; then
             pause
         fi
     fi
@@ -111,7 +112,7 @@ cd $(dirname $([ -x "$(command -v realpath)" ] && realpath "$0" || readlink -f "
 case "$1" in
 start)
         shift
-        start "$@" "$PARAMETERS"
+        start
         exit $?
         ;;
 stop)
@@ -124,7 +125,7 @@ pause)
         ;;
 restart)
         shift
-        stop && (start "$@" "$PARAMETERS")
+        stop; start
         exit $?
         ;;
 try_pause)
